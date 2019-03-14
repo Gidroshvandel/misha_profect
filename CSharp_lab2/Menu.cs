5c9878af545ca7ff79dd3553ebac09478b6b08c0
hamelionink <term11term@gmail.com>
2019-03-14 16:53:56 +0300

    What is This

---
+++ CSharp_lab2/Menu.cs
@@ -0,0 +1,115 @@
+﻿/*
+Лабораторная работа №2
+
+Студент группы 17ИС1
+
+Михаил Никитин Сергеевич
+
+Вариант 1: Разработать динамически подключаемую библиотеку dll, содержащая среднеарифметическое и среднегеометрическое значение одномерных и двумерных массивов.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryMassive;
+
+namespace CSharp_lab2
+{
+    class Menu
+    {
+        static void Main(string[] args)
+        {
+            int number, number_1 = 0;
+            int x, y;
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Одномерный массив: ");
+            Console.WriteLine("2. Двумерный массив: ");
+            Console.WriteLine("3. Выход из Программы");
+            Console.Write("Выберите пункт 1 - 3: ");
+            number = int.Parse(Console.ReadLine());
+            Console.Clear();
+            switch(number)
+            {
+                case 1:
+                    
+                        Console.WriteLine("Введите размерность массива [x] :");
+                        x = int.Parse(Console.ReadLine());
+                        MassiveInfo masive_1 = new MassiveInfo(x, 0, number);
+                        masive_1.InPut();
+                        Console.Clear();
+                    do {
+                        Console.Clear();
+                        Console.WriteLine("1. Среднеарефметическое x");
+                        Console.WriteLine("2. Среднегеометрическое y");
+                        Console.WriteLine("3. Выход из Программы");
+                        Console.Write("Выберите пункт 1 - 3: ");
+                        number_1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        if (number_1 == 1)
+                        {
+                            masive_1.Show();
+                            masive_1.ArithmeticMean();
+                            Console.WriteLine("Нажмите любую кнопку чтобы вернуться");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+
+                        if (number_1 == 2)
+                        {
+                            masive_1.Show();
+                            masive_1.GeometricMean();
+                            Console.WriteLine("Нажмите любую кнопку чтобы вернуться");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+                    } while (number_1 != 3);
+                    break;
+                case 2:
+                    Console.WriteLine("Введите размерность массива [x][y] :");
+                    x = int.Parse(Console.ReadLine());
+                    y = int.Parse(Console.ReadLine());
+                    MassiveInfo masive_2 = new MassiveInfo(x, y, number);
+                    masive_2.InPut();
+                    Console.Clear();
+                    do
+                    {
+                        Console.WriteLine("1. Среднеарефметическое x");
+                        Console.WriteLine("2. Среднегеометрическое y");
+                        Console.WriteLine("3. Выход из Программы");
+                        Console.Write("Выберите пункт 1 - 3: ");
+                        number_1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        if (number_1 == 1)
+                        {
+                            masive_2.Show();
+                            masive_2.ArithmeticMean();
+                            Console.WriteLine("Нажмите любую кнопку чтобы вернуться");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+
+                        if (number_1 == 2)
+                        {
+                            masive_2.Show();
+                            masive_2.GeometricMean();
+                            Console.WriteLine("Нажмите любую кнопку чтобы вернуться");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+                    } while (number_1 != 3);
+                    break;
+                case 3:
+                    Console.ReadKey();
+                    break;
+                default:
+                    if (number > 3 | number < 1)
+                        Console.WriteLine("Ошибка, повторите попытку");
+                    break;
+            }
+        }
+
+        
+    }
+}
---
+++ Library/MassiveInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMassive
+{
+    public class MassiveInfo
+    {
+        private int[] massive_1;
+        private int[,] massive_2;
+
+        public MassiveInfo(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            massive_1 = new int[x, y];
+
+        }
+    }
+}
